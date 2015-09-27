7b3b660f3f9a16e5fb57f3925090385624fb451f
Charlie Tarr <charlietarr1@gmail.com>
2015-09-27 20:53:06 +0100

    adding files for checkpoint 38

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -9,13 +9,10 @@ RSpec.describe Post, type: :model do
 
   it { should belong_to(:topic) }
 
-  describe "attributes" do
-    it "should respond to title" do
-      expect(post).to respond_to(:title)
-    end
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:body) }
+  it { should validate_presence_of(:topic) }
 
-    it "should respond to body" do
-      expect(post).to respond_to(:body)
-    end
-  end
+  it { should validate_length_of(:title).is_at_least(5) }
+  it { should validate_length_of(:body).is_at_least(20) }
 end
