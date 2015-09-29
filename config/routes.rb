43750b7b17c3856c929158144a19a7b1b59b850f
Charlie Tarr <charlietarr1@gmail.com>
2015-09-29 23:15:18 +0100

    Adding files for checkpoint_41

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   end
   resources :users, only: [:new, :create]
 
+  resources :sessions, only: [:new, :create, :destroy]
+
   post "users/confirm" => "users#confirm"
 
   get "about" => "welcome#about"
