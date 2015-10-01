d4006a3a10d9db6d3b5d55491c19278c1876e975
Charlie Tarr <charlietarr1@gmail.com>
2015-10-01 12:27:45 +0100

    Adding exercises for checkpoint 43

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,8 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_many :comments, dependent: :destroy
   default_scope { order('created_at DESC') }
+  scope :ordered_by_title, -> {reorder("title")}
+  scope :ordered_by_reverse_created_at, -> {reorder("created_at ASC")}
 
 
   validates :title, length: { minimum: 5 }, presence: true
