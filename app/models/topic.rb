130de9e6f1c144f9f1f3415b43c5e9fc60e947e4
Charlie Tarr <charlietarr1@gmail.com>
2015-10-26 13:57:55 +0000

    Adding files for checkpoint 46: Private Topics

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -7,6 +7,7 @@ class Topic < ActiveRecord::Base
   has_many :rates, through: :ratings
   has_many :ratings, as: :rateable
 
+  scope :visible_to, -> (user) {user ? all : where(public: true)}
 
   validates :name, length: { minimum: 5 }, presence: true
   validates :description, length: { minimum: 15 }, presence: true
